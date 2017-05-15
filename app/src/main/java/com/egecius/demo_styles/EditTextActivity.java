c0888f9b7be37cb0d7a8e6e5c8d1552c74229891
Egidijus Gecius <e.gecius@gmail.com>
2017-05-15 14:37:09 +0100

    style editexts

---
+++ app/src/main/java/com/egecius/demo_styles/EditTextActivity.java
@@ -0,0 +1,14 @@
+package com.egecius.demo_styles;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditTextActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edittext);
+	}
+}
